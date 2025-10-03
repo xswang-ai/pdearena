6294544648c9cd47cf4bb579b482c1cd065a37e0
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-03 13:46:46 +1000

    Update test_navierstokes_jobs.sh: remove unused module loads and streamline environment activation for improved clarity.

--- test_navierstokes_jobs.sh
+++ test_navierstokes_jobs.sh
@@ -11,12 +11,8 @@
 #SBATCH --cpus-per-task=8       # cpu-cores per task (>1 if multi-threaded tasks)
 
 
-# module load numpy/2.0.0-py312
 module load pytorch/2.5.1-py312-cu122-mpi
-module load parallel python
-# source /scratch3/wan410/venvs/testing/bin/activate
 source $HOME/.venvs/pytorch/bin/activate
-
 
 python3 generate_data.py base=configs/navierstokes2dsmoke_nt5k_fy0.1.yaml  experiment=smoke mode=train samples=5000 seed=197910 \
 dirname=/scratch3/wan410/operator_learning_data/pdearena/NSE-2D-Customised
