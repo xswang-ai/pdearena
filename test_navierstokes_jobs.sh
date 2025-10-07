495544b0b32b2c3ca32bb0cdb44c2bcd01a57783
xuesongwang <xuesong.wang1@unsw.edu.au>
2025-10-07 13:42:24 +1100

    sdfs

--- test_navierstokes_jobs.sh
+++ test_navierstokes_jobs.sh
@@ -14,8 +14,8 @@
 module load pytorch/2.5.1-py312-cu122-mpi
 source $HOME/.venvs/pytorch/bin/activate
 
-python3 generate_data.py base=configs/navierstokes2dsmoke_nt560_tol1e-3.yaml  experiment=smoke mode=train samples=2  seed=197910 \
-dirname=/scratch3/wan410/operator_learning_data/pdearena/NSE-2D-Customised
+# python3 generate_data.py base=configs/navierstokes2dsmoke_nt560_tol1e-3.yaml  experiment=smoke mode=train samples=2  seed=197910 \
+# dirname=/scratch3/wan410/operator_learning_data/pdearena/NSE-2D-Customised
 
 python3 generate_data.py base=configs/navierstokes2dsmoke_nt2800_tol1e-4.yaml  experiment=smoke mode=train samples=2  seed=197910 \
 dirname=/scratch3/wan410/operator_learning_data/pdearena/NSE-2D-Customised
